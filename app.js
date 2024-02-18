a25ea5861780098961f51c6ccb928531dcf2de3b
bipul-eng <bipulkr2792003@gmail.com>
2024-02-18 13:06:30 +0530

    change bg

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ async function fetching()
 const response=await fetch("https://opentdb.com/api.php?amount=10&type=multiple");
 data=await response.json();
 document.getElementById("question").innerHTML=data.results[0].question;
-document.getElementById("btn1").innerHTML=data.results[0].correct_answer;
 document.getElementById("btn2").innerHTML=data.results[0].incorrect_answers[0];
 document.getElementById("btn3").innerHTML=data.results[0].incorrect_answers[1];
+document.getElementById("btn1").innerHTML=data.results[0].correct_answer;
 document.getElementById("btn4").innerHTML=data.results[0].incorrect_answers[2];
 }
 fetching();
