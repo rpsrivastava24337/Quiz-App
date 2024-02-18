ef25847049d909ae9acd9aeb86c817249392e33b
bipul-eng <bipulkr2792003@gmail.com>
2024-02-18 12:57:21 +0530

    js api update

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 //https://opentdb.com/api.php?amount=10&type=multiple
 
-let boxes=document.getElementsByClassName("first");
+let data;
 async function fetching()
 {
 const response=await fetch("https://opentdb.com/api.php?amount=10&type=multiple");
-var data=await response.json();
-console.log(data);
-document.getElementById("btn").innerHTML=data.results[0].question;
+data=await response.json();
+document.getElementById("question").innerHTML=data.results[0].question;
 document.getElementById("btn1").innerHTML=data.results[0].correct_answer;
 document.getElementById("btn2").innerHTML=data.results[0].incorrect_answers[0];
 document.getElementById("btn3").innerHTML=data.results[0].incorrect_answers[1];
@@ -15,6 +14,12 @@ document.getElementById("btn4").innerHTML=data.results[0].incorrect_answers[2];
 fetching();
 function check(event)
 {
-    event.preventDefault();
-    event.target.
+    var data2=event.target.innerHTML;
+    
+    if(data2==data.results[0].correct_answer)
+    {
+        event.target.classList.add("correct");
+        console.log("correct");
+    }
+    
 }
