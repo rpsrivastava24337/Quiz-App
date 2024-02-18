624de34e497b70394b0991c9f75f66ac649bc7f4
bipul-eng <bipulkr2792003@gmail.com>
2024-02-18 11:19:11 +0530

    h

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 //https://opentdb.com/api.php?amount=10&type=multiple
+let apiLink="https://opentdb.com/api.php?amount=10&type=multiple"
+
+async function fetching()
+{
+const response=await fetch(apiLink);
+var data=await response.json();
+console.log(data);
+}
