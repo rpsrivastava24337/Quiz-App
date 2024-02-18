03af5dbd2739cc06af52089fd3d26d05280b8488
bipul-eng <bipulkr2792003@gmail.com>
2024-02-18 12:12:44 +0530

    add api

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 //https://opentdb.com/api.php?amount=10&type=multiple
-let apiLink="https://opentdb.com/api.php?amount=10&type=multiple"
 
+let boxes=document.getElementsByClassName("first");
 async function fetching()
 {
-const response=await fetch(apiLink);
+const response=await fetch("https://opentdb.com/api.php?amount=10&type=multiple");
 var data=await response.json();
 console.log(data);
+document.getElementById("btn").innerHTML=data.results[0].question;
+document.getElementById("btn1").innerHTML=data.results[0].correct_answer;
+document.getElementById("btn2").innerHTML=data.results[0].incorrect_answers[0];
+document.getElementById("btn3").innerHTML=data.results[0].incorrect_answers[1];
+document.getElementById("btn4").innerHTML=data.results[0].incorrect_answers[2];
+}
+fetching();
+function check(event)
+{
+    event.preventDefault();
+    event.target.
 }
